cfe9a25adec934071b149b3952df282b757db97b
Tushar Koul <tusharkoul@gmail.com>
2017-08-23 15:12:41 -0700

    creating basic react native project with ArcGIS ios sdk installed

---
+++ ios/ArcGIS Components/AGSRNMapView.h
@@ -0,0 +1,14 @@
+//
+//  AGSRNMapView.h
+//  ArcGISReactSample
+//
+//  Created by Tushar Koul on 8/23/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ArcGIS/ArcGIS.h>
+
+@interface AGSRNMapView : AGSMapView
+
+@end
---
+++ ios/ArcGIS Components/AGSRNMapView.m
@@ -0,0 +1,13 @@
+//
+//  AGSRNMapView.m
+//  ArcGISReactSample
+//
+//  Created by Tushar Koul on 8/23/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "AGSRNMapView.h"
+
+@implementation AGSRNMapView
+
+@end
