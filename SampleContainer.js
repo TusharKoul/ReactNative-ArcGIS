ed0a24e6bb258ac07c6f8f1141462f2742c6e48a
Tushar Koul <tusharkoul@gmail.com>
2017-08-25 13:31:06 -0700

    adding text input for searching

--- SampleContainer.js
+++ SampleContainer.js
@@ -3,30 +3,68 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
-    View
+    View,
+    TextInput,
 } from 'react-native';
 
 import AGSMapView from './AGSMapView';
 
 export default class SampleContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            typingTimeout: 0
+        };
+    }
+
     render() {
         return (
         <View style={styles.container}>
+            <TextInput  style={styles.searchInput}
+                        placeholder='Search for place'
+                        onChangeText={this.onSearchTextChange} />
             <AGSMapView style={styles.map}/>
         </View>
         );
+    }
+
+    onSearchTextChange = (text) => {
+        let {typingTimeout} = this.state;
+        if(typingTimeout) {
+            console.log('clearing timeout');
+            clearTimeout(typingTimeout);
+        }
+
+        // search after 1 sec of typing
+        let timeout = setTimeout(()=> {
+            console.log('Fire search function with ' + text);
+        },1000);
+
+        this.setState({
+            typingTimeout: timeout
+        });
+
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        padding: 20,
         marginTop: 65,
-        backgroundColor: 'skyblue'
+        backgroundColor: 'gray'
     },
     map:{
-        flex:1,
-        backgroundColor: 'green'
+        flex:1
+    },
+    searchInput:{
+        margin:5,
+        borderColor: 'lightgray',
+        borderWidth: 1,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        height: 30,
+        paddingLeft: 10,
+        paddingRight: 10
     }
 });
